fcd2b0589ddc30666494d34d8b8d874264b04a36
Subhashis Suara <subhashis.suara999@gmail.com>
2021-03-10 02:50:23 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      task: "",
+      tasks: [],
+    };
   }
+
   render() {
-    return <div>Hello!</div>;
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter your task </label>
+          <br />
+          <input type="text" id="taskInput" />
+          <br />
+          <br />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    );
   }
 }
 
