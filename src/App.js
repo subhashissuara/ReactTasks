4356f4a1e91d1b6a6a03e9441d2f1e60679eeac6
Subhashis Suara <subhashis.suara999@gmail.com>
2021-03-10 03:10:17 +0530

    Complete Functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Overview from "./Components/Overview";
 
 class App extends Component {
   constructor() {
@@ -8,21 +9,44 @@ class App extends Component {
       task: "",
       tasks: [],
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitTask = this.onSubmitTask.bind(this);
   }
+
+  handleChange = (change) => {
+    this.setState({
+      task: change.target.value,
+    });
+  };
+
+  onSubmitTask = (task) => {
+    task.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+    });
+  };
 
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter your task </label>
           <br />
-          <input type="text" id="taskInput" />
+          <input
+            type="text"
+            id="taskInput"
+            onChange={this.handleChange}
+            value={this.state.task}
+          />
           <br />
           <br />
           <button type="submit">Add Task</button>
         </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
